38b98415e70bf223afaffa97a3a35debffb1c271
邱雪 <qiuxue_ok@163.com>
2018-07-25 18:19:13 +0800

    modify defineProperty file

--- spec/core/defineProperty.spec.js
+++ spec/core/defineProperty.spec.js
@@ -3,11 +3,14 @@ let {defineReactive} = require('../../src/core/defineProperty.js');
 describe('Object DefineProperty',function () {
    it('should modify object property',function () {
 
-       let obj = {name:'haha'};
+       let obj = {};
+       obj.name = 'haha';
+       // let obj = {name:'haha'};
        let prop = 'name';
-       let descriptor = {name:'123'};
+       let descriptor = '123';
 
-       let result = defineReactive(obj,prop,descriptor);
+       let result = defineReactive(obj,prop,descriptor)[prop];
+
        let expected = '123';
        expect(result).toEqual(expected);
    })
--- src/core/defineProperty.js
+++ src/core/defineProperty.js
@@ -3,26 +3,44 @@
  *         prop:需要定义或修改的属性；
  *         descriptor:将被定义或修改属性的描述符
 */
+
 function defineReactive(obj,prop,descriptor) {
 
-    Object.defineProperty(obj,prop,{
+    return Object.defineProperty(obj,prop,{
         enumerable:true,
         configurable:false,
 
         get:function () {
+            console.log('value  '+ descriptor);
             return descriptor;
         },
         set:function (newVal) {
+            console.log('new value' + newVal);
             descriptor = newVal;
         }
-    })
+    });
 
 }
 
-let obj = {name:'haha'};
-let prop = 'name';
-let descriptor = {name:'123'};
-defineReactive(obj,prop,descriptor);
+let obj = {};
+
+obj.name = 'haha';
+console.log(defineReactive(obj,'name','dd').name);
+var o = {};
+var bValue;
+Object.defineProperty(o, "b", {
+    get : function(){
+        return bValue;
+    },
+    set : function(newValue){
+        bValue = newValue;
+    },
+    enumerable : true,
+    configurable : true
+});
+
+o.b = 38;
+console.log(o.b);
 module.exports = {
     defineReactive
 };
